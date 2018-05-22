02cdd2c733a8e7a8607555eb4d746ecd51076318
Syed Nizamudeen <nizam.programmer@gmail.com>
2018-05-22 23:40:19 +0800

    File Input Plugin Added
    published to vendor folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,11 @@ class AppServiceProvider extends ServiceProvider
             __DIR__ . '/../../vendor/jquery' => public_path('vendor/jquery'),
             __DIR__ . '/../../vendor/twitter/bootstrap/dist' => public_path('vendor/bootstrap/dist'),
             __DIR__ . '/../../vendor/grimmlink/jquery-slimscroll' => public_path('vendor/jquery-slimscroll'),
-            __DIR__ . '/../../vendor/select2/dist' => public_path('vendor/select2/dist')
+            __DIR__ . '/../../vendor/select2/dist' => public_path('vendor/select2/dist'),
+            __DIR__ . '/../../vendor/kartik-v/bootstrap-fileinput/css' => public_path('vendor/bootstrap-fileinput/css'),
+            __DIR__ . '/../../vendor/kartik-v/bootstrap-fileinput/js' => public_path('vendor/bootstrap-fileinput/js'),
+            __DIR__ . '/../../vendor/kartik-v/bootstrap-fileinput/img' => public_path('vendor/bootstrap-fileinput/img'),
+            __DIR__ . '/../../vendor/kartik-v/bootstrap-fileinput/themes' => public_path('vendor/bootstrap-fileinput/themes')
         ], 'public');
     }
 
